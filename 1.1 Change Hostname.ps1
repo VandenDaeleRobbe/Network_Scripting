216bd65b3c79bdac9c759087e84ab3dc12b54db3
unknown <robbe.van.den.daele@student.howest.be>
2020-09-23 11:01:06 +0200

    Exercise 1.1

---
+++ 1.1 Change Hostname.ps1
@@ -0,0 +1,11 @@
+﻿# Get userinput for a new computername
+$newComputerName = Read-Host "New computer name to set"
+$isOk = Read-Host "The system needs to be restarted. Do you want to restart now? (Y/N)"
+
+# Set computername and restart if asked
+if ($isOk -eq "Y") {
+    Rename-Computer -NewName $newComputerName -Restart
+} else {
+    Rename-Computer -NewName $newComputerName
+    Write-Host "Restart the machine for the changes to take place"
+}
