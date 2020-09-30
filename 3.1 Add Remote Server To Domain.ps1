64d9031d4e4a7961e06287d591af9353fca1cddd
unknown <robbe.van.den.daele@student.howest.be>
2020-09-30 09:48:40 +0200

    Edited 2.1 and added 3.1

--- 3.1 Add Remote Server To Domain.ps1
+++ 3.1 Add Remote Server To Domain.ps1
@@ -4,4 +4,4 @@ $dns_root = $domain.DNSRoot
 $computer_name = Read-Host "Give the computername of the machine you want to make a member of the domain"
 
 # Add MS to domain
-Add-Computer -DomainName $dns_root -ComputerName $computer_name -Credential $dns_root\administrator -LocalCredential $computer_name\administrator -Restart
+Add-Computer -DomainName $dns_root -ComputerName $computer_name -Credential $dns_root\administrator -LocalCredential win13-MS\administrator -Restart
