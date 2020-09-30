d3210e7cc54e6c42975593a4a3ac23aea030be8b
unknown <robbe.van.den.daele@student.howest.be>
2020-09-30 09:16:40 +0200

    Edited 1.2 and added 2.1

--- 1.2 Set Static IP Address.ps1
+++ 1.2 Set Static IP Address.ps1
@@ -1,9 +1,10 @@
 ﻿#Get physical network adapter
 $nic=Get-NetAdapter | where {$_.MediaType -eq "802.3"}
+$ip_address = Read-Host "Give the IP address to set"
 
 #Romove all existing instances
 Remove-NetIPAddress -InterfaceAlias $nic.Name -Confirm:$false
 Remove-NetRoute -InterfaceAlias $nic.Name -Confirm:$false
 #Set IP address and dns servers
-New-NetIPAddress -IPAddress 192.168.1.2 -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceAlias $nic.Name
+New-NetIPAddress -IPAddress $ip_address -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceAlias $nic.Name
 Set-DnsClientServerAddress -ServerAddresses ("172.20.0.2", "172.20.0.3") -InterfaceAlias $nic.Name
---
+++ 2.1 Add Remote Server To Domain.ps1
@@ -0,0 +1,7 @@
+﻿# Variables
+$domain = Get-ADDomain
+$dns_root = $domain.DNSRoot
+$computer_name = "win13-MS"
+
+# Add MS to domain
+Add-Computer -DomainName $dns_root -ComputerName $computer_name -Credential $dns_root\administrator -LocalCredential win13-MS\administrator -Restart
