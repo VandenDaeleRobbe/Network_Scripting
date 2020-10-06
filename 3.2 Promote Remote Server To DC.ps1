cdedf644575f89239959eea9c5e470d5a44986ba
unknown <robbe.van.den.daele@student.howest.be>
2020-10-06 09:36:16 +0200

    Added 3.2 and 3.3

---
+++ 3.2 Promote Remote Server To DC.ps1
@@ -0,0 +1,23 @@
+﻿#
+# Get credentials and start session
+#
+$cred = Get-Credential -Message "Local remote server credential"
+$sess = New-PSSession -Credential $cred -ComputerName "WIN13-DC2"
+Enter-PSSession $sess
+
+
+#
+# Configure server as DC and GC
+#
+Invoke-Command -Session $sess {
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+    $domain_cred = Get-Credential -Message "Domain credentials to promote DC"
+    Install-ADDSDomainController -InstallDns -Credential $domain_cred -DomainName "intranet.mijnschool.be" -Force
+}
+
+
+#
+# Exit session
+#
+Exit-PSSession
+Remove-PSSession $sess
---
+++ 3.3 Configure Remote Server As DHCP Replication Partner.ps1
@@ -0,0 +1,37 @@
+﻿#
+# Fixed variables
+#
+$DNS_NAME_DC1 = "win13-DC1.intranet.mijnschool.be"
+$DNS_NAME_DC2 = "win13-DC2.intranet.mijnschool.be"
+$IP_ADDRESS_DC2 = "192.168.1.3"
+$SCOPE_ID = "192.168.1.0"
+
+
+#
+# Get credentials and start session
+#
+$cred = Get-Credential -Message "Credentials of server to configure"
+$sess = New-PSSession -Credential $cred -ComputerName "WIN13-DC2"
+Enter-PSSession $sess
+
+
+#
+# Install DHCP service and set replication partner
+#
+Invoke-Command -Session $sess {
+    Install-WindowsFeature DHCP -IncludeManagementTools
+    Add-DhcpServerInDC -DnsName $using:DNS_NAME_DC2 -IPAddress $using:IP_ADDRESS_DC2 # $using: is needed to use the local variables in remote session
+}
+
+
+#
+# Exit session
+#
+Exit-PSSession
+Remove-PSSession $sess
+
+
+#
+# Set server as replication partner
+#
+Add-DhcpServerv4Failover -ComputerName $DNS_NAME_DC1 -Name "DC2-Failover"-PartnerServer $DNS_NAME_DC2 -ScopeId $SCOPE_ID -SharedSecret "P@ssw0rd"
