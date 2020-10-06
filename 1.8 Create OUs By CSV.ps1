c9d3f57ad12d60cadb1772d40c07d467cbe2badd
unknown <robbe.van.den.daele@student.howest.be>
2020-10-06 09:55:07 +0200

    Added 1.8

---
+++ 1.8 Create OUs By CSV.ps1
@@ -0,0 +1,12 @@
+﻿#
+# Read CSV file
+#
+$FILE = Import-Csv -Path "C:\Users\Administrator\Desktop\Network Scripting\Network_Scripting\Docs\OUs.csv" -Delimiter ";"
+
+
+#
+# Create OU for each line
+#
+$FILE | ForEach-Object {
+    New-ADOrganizationalUnit -Name $_.Name -Path $_.Path -DisplayName $_.Display_Name -Description $_Description
+}
