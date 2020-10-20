8c1053bee62ebd89b07a3b80323c2d89716d9699
unknown <robbe.van.den.daele@student.howest.be>
2020-10-20 09:48:59 +0200

    Finished Powershell 2

---
+++ Powershell 2/3.2 Add NPS Clients.ps1
@@ -0,0 +1,21 @@
+﻿#
+# Install NPS
+#
+Install-WindowsFeature NPAS -IncludeManagementTools
+
+#
+# Read CSV
+#
+$file = Import-Csv -Path "C:\Users\Administrator\Desktop\Network Scripting\Network_Scripting\Docs\RadiusClients.csv" -Delimiter ";"
+$file | Format-Table
+
+#
+# Create radius clients
+#
+$FILE | ForEach-Object {
+    if ($_.Vendor -eq "Standard") {
+        New-NpsRadiusClient -Name $_.Name -Address $_.Address -SharedSecret $_.SharedSecret
+    } else {
+        New-NpsRadiusClient -Name $_.Name -Address $_.Address -SharedSecret $_.SharedSecret -VendorName $_.Vendor
+    }
+}
