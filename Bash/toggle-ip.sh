f8648ae25e88739f93b7f06a1d96f55e59585e7a
root <robbe.van.den.daele@student.howest.be>
2020-10-21 10:32:17 +0200

    Edited toggle IP script

--- Bash/toggle-ip.sh
+++ Bash/toggle-ip.sh
@@ -4,20 +4,21 @@
 # --------------------
 interface=$1
 config_path="/etc/network/interfaces"
-static_address="192.168.40.129/24"
-static_gateway="192.168.40.2"
+static_address="192.168.1.5/24"
+static_gateway="192.168.1.1"
+seconds_date=$(date +'%s')
 
 
 # --------------------
 # Create backup of interfaces file
 # --------------------
-cp $config_path ~/interfaces.back
+cp $config_path $config_path.$seconds_date.back
 
 
 # --------------------
 # Check if interface exists
 # --------------------
-ip a | grep -p $interface
+ip a | grep -q $interface
 if [[ $? -ne 0 ]]; then
 	echo "Interface does not exists!"
 	exit 1
@@ -27,16 +28,8 @@ fi
 # --------------------
 # Check if current config is static or dynamic
 # --------------------
-grep -p "iface $interface inet dhcp" $config_path
+grep -q "iface $interface inet dhcp" $config_path
 is_dhcp=$?
-
-#rm -f $config_path # Remove current config file
-#touch $config_path # Create config file
-
-# Add loopback
-#echo "iface lo inet loopback" >> $config_path
-#echo "auto lo" >> $config_path
-#echo "" >> $config_path
 
 
 # --------------------
@@ -46,10 +39,8 @@ if [[ $is_dhcp -eq 0 ]]; then
 	echo "Configuration is dynamic and will be set to static!"
 
 	# Add commands
-	#echo "auto $interface" >> $config_path
-	#echo "iface $interface inet static" >> $config_path	
-	sed -i "s/inet dhcp/inet static" $config_path
-	echo -e "\taddress $static_address/24" >> $config_path
+	sed -i "s/inet dhcp/inet static/" $config_path
+	echo -e "\taddress $static_address" >> $config_path
 	echo -e "\tgateway $static_gateway" >> $config_path
 fi
 
@@ -61,11 +52,9 @@ if [[ $is_dhcp -ne 0 ]]; then
 	echo "Configuration is static and will be set to dynamic!"
 
 	# Add commands
-	#echo "auto $interface" >> $config_path
-	#echo "iface $interface inet dhcp" >> $config_path	
-	sed -i "s/inet static/inet dhcp" $config_path
-	sed -i "s/address/d" $config_path
-	sed -i "s/gateway/d" $config_path
+	sed -i "s/inet static/inet dhcp/" $config_path
+	sed -i "/address/d" $config_path
+	sed -i "/gateway/d" $config_path
 fi
 
 
@@ -73,4 +62,12 @@ fi
 # restart service
 # --------------------
 systemctl restart networking.service
-exit $?
+restart_ok=$?
+if [[ $restart_ok -eq 0 ]]; then
+	echo "Network service restarted succesfully"
+	rm -f $config_path.$seconds_date.back
+	exit $restart_ok
+else 
+	echo "Network service could not be restarted due to errors! There is a backupfile made in /etc/network/."
+	exit $restart_ok
+fi
